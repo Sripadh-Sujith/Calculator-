567f0843cbc298c23bd831f7ea548915c6db8646
Sripadh-Sujith <sripadhsujith2009@gmail.com>
2025-02-06 20:48:33 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,86 @@
+
+
+
+function cal() {
+    let val=document.getElementById("inp").value;
+    let eva=eval(val);
+    let x=eva.toString();
+    let y=document.getElementById("inp").value=eva;
+    
+    
+}
+
+function one() {
+    let val=document.getElementById("inp");
+    val.value+='1';
+    
+}
+function two() {
+    let val=document.getElementById("inp");
+    val.value+='2';
+    
+}
+function three() {
+    let val=document.getElementById("inp");
+    val.value+='3';
+    
+}
+function four() {
+    let val=document.getElementById("inp");
+    val.value+='4';
+    
+}
+function five() {
+    let val=document.getElementById("inp");
+    val.value+='5';
+    
+}
+function six() {
+    let val=document.getElementById("inp");
+    val.value+='6';
+    
+}
+function seven() {
+    let val=document.getElementById("inp");
+    val.value+='7';
+    
+}
+function eight() {
+    let val=document.getElementById("inp");
+    val.value+='8';
+    
+}
+function nine() {
+    let val=document.getElementById("inp");
+    val.value+='9';
+    
+}
+function zero() {
+    let val=document.getElementById("inp");
+    val.value+='0';
+    
+}
+function multi() {
+    let val=document.getElementById("inp");
+    val.value+='*';
+    
+}
+function divide() {
+    let val=document.getElementById("inp");
+    val.value+='/';
+    
+}
+function sub() {
+    let val=document.getElementById("inp");
+    val.value+='-';
+    
+}
+function add() {
+    let val=document.getElementById("inp");
+    val.value+='+';
+    
+}
+function clr(){
+    let val=document.getElementById("inp");
+    val.value=' ';
+}
